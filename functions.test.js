be14be8e5c8531ebf479bc3c088215e03aecf636
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-04-22 17:25:26 -0300

    Update functions.test.js

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const { add } = require('./functions.js');
+const { add, isNull, checkValue } = require('./functions.js');
 
 test('Adds 2 + 2 equal 4', () => {
     expect(add(2, 2)).toBe(4);
@@ -6,4 +6,12 @@ test('Adds 2 + 2 equal 4', () => {
 
 test('Add 2 + 2 NOT equal 5', () => {
     expect(add(2, 2)).not.toBe(5);
+});
+
+test('Should be null', () => {
+    expect(isNull()).toBeNull();
+});
+
+test('Should be falsy', () => {
+    expect(checkValue(null)).toBeFalsy();
 });
