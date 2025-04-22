ac6e9a7c3894bd82c89beee35726722b3250a7b3
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-04-22 17:30:17 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,13 @@
 const functions = {
     add: (num1, num2) => num1 + num2,
     isNull: () => null,
-    checkValue: x => x
+    checkValue: x => x,
+    createUser: () => {
+        const user = { firstName: 'Joe' };
+        user['lastName'] = 'Doe';
+        
+        return user;
+    }
 };
 
 module.exports = functions;
